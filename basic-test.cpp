a0903639cdcbc1cbbc57095a46885d022d96d16e
Luca Marchesi <luca.marchesi@peoplefone.com>
2018-11-27 20:55:01 +0100

    new test for code coverage

--- basic-test.cpp
+++ basic-test.cpp
@@ -1,9 +1,14 @@
+#include "header.h"
 #include <gmock/gmock.h>
 
-using namespace::testing;
-using namespace SlidingTiles;
+
+using namespace ::testing;
 
 TEST(Basic, Initial) {
     int a {0};
-    ASSERT_THAT(a, 0)
+    ASSERT_THAT(a, 0);
+}
+
+TEST(Basic, Sum) {
+    ASSERT_EQ(5, sum(3, 2));
 }
