08cd62843358e3e94cff715597c95f7c370b3fa2
Luca Marchesi <luca.marchesi@peoplefone.com>
2018-11-27 20:06:56 +0100

    add buggy code

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
+#include <vector>
 
 
+
+void breaker()
+{
+    std::vector<int> vec {1,2,3};
+    for (int i=0; i<=vec.size(); i++) {
+        std::cout << "never happening.?" << vec[i] << " ";
+    }
+    
+}
 
 
 int main()
 {
     std::cout << "Hello CI" << std::endl;
+    
+    
+    breaker();
+    
     return 0;
 }
