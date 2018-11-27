9216adc8cbe60f97d92bc99801a2c6f8d4106910
Luca Marchesi <luca.marchesi@peoplefone.com>
2018-11-27 19:46:30 +0100

    initial commit

---
+++ basic-test.cpp
@@ -0,0 +1,9 @@
+#include <gmock/gmock.h>
+
+using namespace::testing;
+using namespace SlidingTiles;
+
+TEST(Basic, Initial) {
+    int a {0};
+    ASSERT_THAT(a, 0)
+}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+
+
+
+int main()
+{
+    std::cout << "Hello CI" << std::endl;
+    return 0;
+}
